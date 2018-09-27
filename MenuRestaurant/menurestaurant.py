6befc2a5981b7ce7da82978d341228f6247776ee
Cristobal Muñoz <cmunoz21x@gmail.com>
2018-09-27 16:52:40 -0400

    ejercicio 1 terminado

--- MenuRestaurant/menurestaurant.py
+++ MenuRestaurant/menurestaurant.py
@@ -1,5 +1,6 @@
 import re
-# import only system from os 
+import uuid
+import time
 from os import system, name 
 
 class Restaurant:
@@ -20,9 +21,10 @@ class MenuItem:
         self.price = price
 
 class Order:
-    def __init__(self, restaurant="", MenuItems = []):
+    def __init__(self, restaurant="", MenuItems = [], tip = None):
         self.restaurant = restaurant
         self.menuItems = MenuItems
+        self.tip = tip
     
     def setOrder(self, MenuItems):
         self.menuItems = MenuItems
@@ -36,10 +38,34 @@ class Order:
             patron = re.compile('\$')
             total += float(patron.sub('',meal.price))
         return total   
+    
+    def setTip(self, tip):
+        self.tip = tip
 
-    def createOrderFile(self):
-        f = open('miorder.txt', 'w')
-        f.write("holaaaaaa")
+    def createOrderFile(self, tip):
+        uid = uuid.uuid4()
+        patron = re.compile('-')
+        code = patron.split(str(uid))[0]
+
+        patron = re.compile('\n')
+        restaurant = patron.sub('', self.restaurant)
+        f = open("order_"+code+'_'+restaurant+'_'+time.strftime("%d%m%Y")+'_'+time.strftime("%H%M%S")+'.txt', 'w')
+        f.write(self.restaurant)
+        f.write("Date: "+time.strftime("%d/%m/%Y")+'\n')
+        f.write("\n=========================================================")
+        for meal in self.menuItems:
+            f.write("\n"+meal.category.ljust(20)+meal.meal_name.ljust(30)+meal.price.ljust(50))
+        f.write("\n=========================================================")
+        subtotal = self.getTotalOrder()
+        tax = round(subtotal * 0.07, 2)
+            
+        total = subtotal + tax + float(tip)
+        f.write("\n\nSubTotal".ljust(11)+"= $"+str(subtotal))
+        f.write("\nTax".ljust(11)+"= $"+str(tax))
+        f.write("\nTip".ljust(11)+"= $"+str(tip))
+        f.write("\nTotal".ljust(11)+"= $"+str(total))
+        f.close()
+
 
 def getMenuItem(menuObjects):
         category = menuObjects[0]
@@ -95,9 +121,12 @@ def groupByCategory(menuItems):
     return menuItemsGroup
 
 def showMeals(category):
-    print(category[0].category)
+    print('\n')
+    print("---------------------------------------------------------------------")
+    print(category[0].category.upper())
+    print("---------------------------------------------------------------------")
     for meal in category:
-        print(str(meal.index)+'.     '+meal.meal_name+'\t\t\t\t\t\t\t\t'+meal.price)
+        print(str(meal.index).ljust(5)+meal.meal_name.ljust(50)+meal.price.ljust(30))
 
 def readRestaurantFile():
     f = open ('file.txt','r')
@@ -154,19 +183,18 @@ while(opcion != 0):
     clear_console()
     for index in range(0,len(restaurants)):
         print(str((index+1))+". "+restaurants[index].name)
-    print("0. Quit")
-    opcion = int(input("ingrese la opcion del menu del restaurante que desee: "))
+    print("0. Quit\n")
+    opcion = int(input("Please enter to option: "))
     if(opcion != 0):
         clear_console()
-        
         restaurant = restaurants[opcion-1]
-        print("Welcome to "+restaurant.name)
         order = Order(restaurant.name)
-        
         meal_option = None
+        key = False
         while(meal_option != 'q' and meal_option != 'Q'):
             clear_console()
-            print("NewMeal(n)                           Quit(q)")
+            print("WELCOME TO "+restaurant.name)
+            print("NewMeal(n)                           Quit(q)".rjust(58))
             menuItems = groupByCategory(restaurant.Menu.menuItems)
 
             appetizers = menuItems['Appetizers']
@@ -184,22 +212,44 @@ while(opcion != 0):
                showMeals(deserts)
 
             if(len(order.menuItems) > 0):
-                print("================================================")
+                print("\n=====================================================================")
                 print("Your current meal has "+str(len(order.menuItems))+" item(s).")
                 for meal in order.menuItems:
-                    print(str(meal.index)+"\t"+meal.category+"\t\t"+meal.meal_name+"\t\t\t"+meal.price)
-                print("================================================")
-                print("\t\t\t\t\t\tTotal = "+str(order.getTotalOrder()))
-            meal_option = input("Please enter item[1.."+str(lenIndex(menuItems))+"]: ")
-            
+                    print(str(meal.index).ljust(5)+meal.category.ljust(20)+meal.meal_name.ljust(30)+meal.price.ljust(50))
+                print("=====================================================================")
+                subtotal = order.getTotalOrder()
+                tax = round(subtotal * 0.07, 2)
+                print("SubTotal = $".rjust(56)+str(subtotal))
+                print("Tax = $".rjust(56)+str(tax))
+                total = 0
+                if(order.tip != None):
+                    total += float(order.tip) 
+                    print("Tip = $".rjust(56)+str(order.tip))
+                total = subtotal + tax 
+                print("Total = $".rjust(56)+str(total))
+            if(key):
+                key = False
+                print("\nYour ticket has create!!")
+
+            meal_option = input("\nOptions: \n [1.."+str(lenIndex(menuItems))+"]: Add meals to order \n [q]: Quit \n [n]: Generate order \n\nPlease enter item: ")
 
             if(re.match('\d',meal_option)):
                 if(int(meal_option) > 0 and int(meal_option) <= lenIndex(menuItems)):
                     order.addMeal(getMeal(meal_option, menuItems))
-                    #print(getMeal(meal_option, menuItems).meal_name)
             elif(re.match('n|N', meal_option)):
+                tip = None
+                while(True):
+                    tip = input("Please enter TIP: ")
+                    if(re.match('\d',tip)):
+                        order.setTip(tip)
+                        break
+                    else:
+                        print('Tip invalid')
                 if(len(order.menuItems) > 0):
-                    order.createOrderFile()
+                    order.createOrderFile(tip)
+                    key = True
+                else:
+                    exit = input("You don't has select any meal, press any key for continue...")
             elif(re.match('q|Q', meal_option)):
                     pass
             else:
